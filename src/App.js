bfd62da2ed3f2b989c7ffa9772fe491c54e33512
Chaitu <chaitu.ca539@gmail.com>
2020-10-09 02:58:32 +0530

    create and style message component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,53 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import Message from "./components/message/message.component";
 
 function App() {
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState(["sadfs", "asdfsdf", "dsfgdgas"]);
+  const [messages, setMessages] = useState([
+    { username: "Chaitu", text: "Hey Guys!" },
+    { username: "Krishna", text: "Hi Chaitu :)" },
+  ]);
+  const [username, setUsername] = useState("");
 
-  console.log(messages);
+  useEffect(() => {
+    setUsername(prompt("Please enter your name"));
+  }, []);
 
   const sendMessage = (event) => {
+    event.preventDefault();
     // logic to send the message
-    setMessages([...messages, input]);
+    setMessages([...messages, { username: username, text: input }]);
     setInput("");
   };
 
   return (
     <div className="App">
       <h1>Facebook Messenger Clone</h1>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-        }}
-        action=""
-      >
-        <input
-          onChange={(event) => setInput(event.target.value)}
-          value={input}
-        />
-        <button type="submit" onClick={sendMessage}>
-          Send Message
-        </button>
+      <h2>Welcome {username}</h2>
+      <form action="">
+        <FormControl>
+          <InputLabel htmlFor="send-message">Enter a message...</InputLabel>
+          <Input
+            id="send-message"
+            onChange={(event) => setInput(event.target.value)}
+            value={input}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            onClick={sendMessage}
+            disabled={!input}
+          >
+            Send Message
+          </Button>
+        </FormControl>
       </form>
 
       {messages.map((message) => (
-        <p>{message}</p>
+        <Message username={username} message={message} />
       ))}
     </div>
   );
---
+++ src/components/message/message.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./message.styles.scss";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+
+function Message({ message, username }) {
+  const isUser = username === message.username;
+  return (
+    <Card className={`${isUser ? "message__user" : ""} message__card`}>
+      <CardContent>
+        <Typography variant="h5">{message.text}</Typography>
+        <Typography color="textSecondary">{message.username}</Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default Message;
