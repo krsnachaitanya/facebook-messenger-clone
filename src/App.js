2d3e313e1125fd377246b0f34b7b47862c17d9dd
Chaitu <chaitu.ca539@gmail.com>
2020-10-09 01:20:50 +0530

    Add input and for sending messages and map through the messages array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState(["sadfs", "asdfsdf", "dsfgdgas"]);
+
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    // logic to send the message
+    setMessages([...messages, input]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      Facebook Messenger Clone
+      <h1>Facebook Messenger Clone</h1>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+        action=""
+      >
+        <input
+          onChange={(event) => setInput(event.target.value)}
+          value={input}
+        />
+        <button type="submit" onClick={sendMessage}>
+          Send Message
+        </button>
+      </form>
+
+      {messages.map((message) => (
+        <p>{message}</p>
+      ))}
     </div>
   );
 }
